3ac3980472f22d873cd221487233f2053eb5654a
Agustin Amato <agustin.amato96@gmail.com>
2021-01-30 21:16:08 -0300

    defined factories

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -2,18 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Ticket;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class TicketFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Ticket::class;
 
     /**
      * Define the model's default state.
@@ -23,8 +22,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'code' => $this->faker->regexify('[A-Z]{5}[0-4]{3}'),
+            'active' => $this->faker->boolean(),
         ];
     }
 }
